c4abcb42f07a3de94538d60b7acd95b7acd14fec
LeConsulat2 <wooine2@gmail.com>
2024-06-21 21:27:43 +1200

    9.4 Formatter Prompt
    Lets review this later...

--- pages/02_QuizGPT.py
+++ pages/02_QuizGPT.py
@@ -1,10 +1,10 @@
-from langchain.document_loaders import UnstructuredFileLoader
+from langchain_community.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.callbacks import StreamingStdOutCallbackHandler
 import streamlit as st
-from langchain.retrievers import WikipediaRetriever
+from langchain_community.retrievers import WikipediaRetriever
 
 
 st.set_page_config(
@@ -22,7 +22,173 @@ llm = ChatOpenAI(
 )
 
 
-@st.cache_resource(show_spinner="Loading file...")
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
+questions_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+    You are a helpful assistant that is role playing as a teacher.
+         
+    Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
+    
+    Each question should have 4 answers, three of them must be incorrect and one should be correct.
+         
+    Use (o) to signal the correct answer.
+         
+    Question examples:
+         
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+         
+    Question: What is the capital or Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+         
+    Question: When was Avatar released?
+    Answers: 2007|2001|2009(o)|1998
+         
+    Question: Who was Julius Caesar?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+         
+    Your turn!
+         
+    Context: {context}
+""",
+        )
+    ]
+)
+
+questions_chain = {"context": format_docs} | questions_prompt | llm
+
+formatting_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+    You are a powerful formatting algorithm.
+     
+    You format exam questions into JSON format.
+    Answers with (o) are the correct ones.
+     
+    Example Input:
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+         
+    Question: What is the capital or Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+         
+    Question: When was Avatar released?
+    Answers: 2007|2001|2009(o)|1998
+         
+    Question: Who was Julius Caesar?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+    
+     
+    Example Output:
+     
+    ```json
+    {{ "questions": [
+            {{
+                "question": "What is the color of the ocean?",
+                "answers": [
+                        {{
+                            "answer": "Red",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Yellow",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Green",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Blue",
+                            "correct": true
+                        }}
+                ]
+            }},
+            {{
+                "question": "What is the capital or Georgia?",
+                "answers": [
+                        {{
+                            "answer": "Baku",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Tbilisi",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "Manila",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Beirut",
+                            "correct": false
+                        }}
+                ]
+            }},
+            {{
+                "question": "When was Avatar released?",
+                "answers": [
+                        {{
+                            "answer": "2007",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "2001",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "2009",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "1998",
+                            "correct": false
+                        }}
+                ]
+            }},
+            {{
+                "question": "Who was Julius Caesar?",
+                "answers": [
+                        {{
+                            "answer": "A Roman Emperor",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "Painter",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Actor",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Model",
+                            "correct": false
+                        }}
+                ]
+            }}
+        ]
+     }}
+    ```
+    Your turn!
+    Questions: {context}
+""",
+        )
+    ]
+)
+
+formatting_chain = formatting_prompt | llm
+
+
+@st.cache_data(show_spinner="Loading file...")
 def split_file(file):
     file_content = file.read()
     file_path = f"./.cache/quiz_files/{file.name}"
@@ -38,10 +204,6 @@ def split_file(file):
     return docs
 
 
-def format_docs(docs):
-    return "\n\n".join(document.page_content for document in docs)
-
-
 with st.sidebar:
     docs = None
     choice = st.selectbox(
@@ -53,7 +215,7 @@ with st.sidebar:
     )
     if choice == "File":
         file = st.file_uploader(
-            "Upload a .docx, .txt or .pdf file",
+            "Upload a .docx , .txt or .pdf file",
             type=["pdf", "txt", "docx"],
         )
         if file:
@@ -62,9 +224,8 @@ with st.sidebar:
         topic = st.text_input("Search Wikipedia...")
         if topic:
             retriever = WikipediaRetriever(top_k_results=5)
-            with st.spinner("Searching Wikipedia..."):
+            with st.status("Searching Wikipedia..."):
                 docs = retriever.get_relevant_documents(topic)
-
 if not docs:
     st.markdown(
         """
@@ -76,45 +237,12 @@ if not docs:
     """
     )
 else:
-
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                """
-    You are a helpful assistant that is role playing as a teacher.
-         
-    Based ONLY on the following context, make 10 questions to test the user's knowledge about the text.
-    
-    Each question should have 4 answers, three of them must be incorrect and one should be correct.
-         
-    Use (o) to signal the correct answer.
-         
-    Question examples:
-         
-    Question: What are supports provided from student hub?
-    Answers: Financial Assistance(o)|Enrolment processing|Application assessment|Fees refund
-         
-    Question: Where can students find academic support resources?
-    Answers: Library(o)|Cafeteria|Gym|Parking lot
-         
-    Question: Who can assist with career guidance?
-    Answers: Career Advisor(o)|Sports Coach|Security Officer|IT Technician
-         
-    Question: What service helps students with personal issues?
-    Answers: Counseling(o)|Bookstore|Administration Office|Maintenance
-         
-    Your turn!
-         
-    Context: {context}
-""",
-            )
-        ]
-    )
-
-    chain = {"context": format_docs} | prompt | llm
-
     start = st.button("Generate Quiz")
 
     if start:
-        chain.invoke(docs)
+        questions_response = questions_chain.invoke(docs)
+        st.write(questions_response.content)
+        formatting_response = formatting_chain = formatting_chain.invoke(
+            {"context": questions_response.content}
+        )
+        st.write(formatting_response.content)
