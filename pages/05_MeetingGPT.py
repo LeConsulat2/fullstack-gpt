cd99d120b0a697e7cba64dc0f50d24249d0951a3
LeConsulat2 <wooine2@gmail.com>
2024-06-24 22:12:15 +1200

    Update 05_MeetingGPT.py

--- pages/05_MeetingGPT.py
+++ pages/05_MeetingGPT.py
@@ -1,1 +1,28 @@
 import streamlit as st
+
+
+st.set_page_config(
+    page_title="Site",
+    page_icon="📃",
+)
+
+st.title("MeetingGPT")
+
+st.markdown(
+    """
+    ## Welcome to MeetingGPT
+
+    Experience seamless transcription and summary of your meetings with MeetingGPT. 
+    
+    Upload your video, and we'll provide a detailed transcript, a concise summary, and a chatbot to assist with any queries you might have.
+
+    Get started by uploading a video file via the sidebar.
+    """
+)
+
+
+with st.sidebar:
+    video = st.file_uploader(
+        "Video",
+        type=["mp4", "avi", "mkv", "mov"],
+    )
