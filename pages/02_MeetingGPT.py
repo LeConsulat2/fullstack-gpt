0c63f7696ca48d92363aa6e2b8e064601d4f5cf4
LeConsulat2 <wooine2@gmail.com>
2024-07-02 20:59:18 +1200

    Update 02_MeetingGPT.py

--- pages/02_MeetingGPT.py
+++ pages/02_MeetingGPT.py
@@ -50,13 +50,13 @@ if "environment" in st.secrets:
     os.environ["PATH"] = st.secrets["environment"]["PATH"]
 
 # Add the specific FFmpeg path to the PATH environment variable
-ffmpeg_path = "/app/bin/ffmpeg"  # Adjust based on your project structure
-if ffmpeg_path not in os.environ["PATH"]:
-    os.environ["PATH"] = ffmpeg_path + os.pathsep + os.environ["PATH"]
+# ffmpeg_path = "/app/bin/ffmpeg"  # Adjust based on your project structure
+# if ffmpeg_path not in os.environ["PATH"]:
+#     os.environ["PATH"] = ffmpeg_path + os.pathsep + os.environ["PATH"]
 
 # Display the full PATH environment variable
-st.write("Full PATH environment variable:")
-st.write(os.environ["PATH"])
+# st.write("Full PATH environment variable:")
+# st.write(os.environ["PATH"])
 
 
 # Function to check if FFmpeg is installed
