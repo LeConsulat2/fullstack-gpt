6bf5213206f86ccce261427b765b36460b27b3a3
LeConsulat2 <wooine2@gmail.com>
2024-09-16 22:54:31 +1200

    Correction - added to privategpt not sitegpt, so changed

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -3,125 +3,91 @@ import asyncio
 import nest_asyncio
 import os
 from langchain.document_loaders import SitemapLoader
-from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
 from bs4 import BeautifulSoup
-
-# import html2text
 from dotenv import load_dotenv
-from Dark import set_page_config
 from Utils import check_authentication  # Import the utility function
 
+# Set page configuration for Streamlit
 st.set_page_config(
     page_title="Site",
     page_icon="📃",
 )
 
-# Under construction message
-st.markdown(
-    """
-    ## This page is under construction 🔨
-    Working on it, once fixed, this page will be up and running for you to use.
-"""
-)
 
 # Ensure the user is authenticated
 check_authentication()
 
+# Load environment variables
 load_dotenv()
 
 # Access secrets in Streamlit Cloud or locally from environment variables
 openai_api_key = (
     os.getenv("OPENAI_API_KEY") or st.secrets["credentials"]["OPENAI_API_KEY"]
 )
-alpha_vantage_api_key = (
-    os.getenv("ALPHA_VANTAGE_API_KEY")
-    or st.secrets["credentials"]["ALPHA_VANTAGE_API_KEY"]
-)
-username = os.getenv("username") or st.secrets["credentials"]["username"]
-password = os.getenv("password") or st.secrets["credentials"]["password"]
 
-# Set the PATH environment variable from secrets
+# Set PATH environment variable if specified in secrets
 if "environment" in st.secrets:
     os.environ["PATH"] = st.secrets["environment"]["PATH"]
 
 
-# def parse_page(soup: BeautifulSoup):
-#     # Exclude common repetitive elements
-#     for element in soup(["header", "footer", "nav", "aside", "script", "style"]):
-#         element.decompose()
-#     # Extract main content
-#     main_content = soup.find("main")
-#     if main_content:
-#         text = main_content.get_text(separator="\n", strip=True)
-#     else:
-#         text = soup.get_text(separator="\n", strip=True)
-#     return str(text).replace("\n", " ").replace("\xa0", " ")  # Non-breaking space
+# Define the page parsing function
+def parse_page(soup: BeautifulSoup):
+    for element in soup(["header", "footer", "nav", "aside", "script", "style"]):
+        element.decompose()
+    main_content = soup.find("main")
+    text = (
+        main_content.get_text(separator="\n", strip=True)
+        if main_content
+        else soup.get_text(separator="\n", strip=True)
+    )
+    return str(text).replace("\n", " ").replace("\xa0", " ")
 
 
-# @st.cache_data(show_spinner="Loading website...")
-# def load_website(url, limit=100):
-#     splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-#         chunk_size=1000,
-#         chunk_overlap=200,
-#     )
-#     loader = SitemapLoader(
-#         url,
-#         filter_urls=[
-#             r"^(.*\/students\/).*",  # Adjust this regex to be more specific if needed
-#         ],
-#         parsing_function=parse_page,
-#     )
-#     loader.requests_per_second = 2
-#     docs = loader.load_and_split(text_splitter=splitter)[
-#         :limit
-#     ]  # Limit the number of documents
-#     vector_store = FAISS.from_documents(docs, OpenAIEmbeddings())
-#     return vector_store.as_retriever()
+# Function to load and split sitemap data
+@st.cache_data(show_spinner="Loading website...")
+def load_website(url, limit=100):
+    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000, chunk_overlap=200
+    )
+    loader = SitemapLoader(
+        url, filter_urls=[r"^(.*\/about\/).*"], parsing_function=parse_page
+    )  # Adjust filter as needed
+    loader.requests_per_second = 2
+    docs = loader.load_and_split(text_splitter=splitter)[
+        :limit
+    ]  # Limit the number of documents
+    vector_store = FAISS.from_documents(docs, OpenAIEmbeddings())
+    return vector_store.as_retriever()
 
 
-# # Apply nest_asyncio to allow asyncio.run() within an existing event loop
-# nest_asyncio.apply()
+# Apply nest_asyncio to allow asyncio.run() within an existing event loop
+nest_asyncio.apply()
 
-# # Set the event loop policy for Windows
-# if hasattr(asyncio, "WindowsProactorEventLoopPolicy"):
-#     asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+# Set the event loop policy for Windows
+if hasattr(asyncio, "WindowsProactorEventLoopPolicy"):
+    asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
 
+# Streamlit user interface
+st.title("SiteGPT")
+st.markdown(
+    "Welcome! Use this chatbot to ask questions to an AI about the content of a website! Upload your files on the sidebar."
+)
 
-# st.title("SiteGPT")
+with st.sidebar:
+    url = st.text_input(
+        "Write down a URL",
+        placeholder="https://www.manukau.ac.nz/about/site-help/site-map",
+    )
 
-# st.markdown(
-#     """
-#     Welcome!
-
-#     Use this chatbot to ask questions to an AI about the content of a website!
-
-#     Upload your files on the sidebar.
-#     """
-# )
-
-# with st.sidebar:
-#     url = st.text_input(
-#         "Write down a URL",
-#         placeholder="https://example.com/sitemap.xml",
-#     )
-
-
-# if url:
-#     if ".xml" not in url:
-#         with st.sidebar:
-#             st.error("Please write down a Sitemap URL")
-#     else:
-#         documents = load_website(url, limit=100)  # Limit to 100 URLs
-#         if documents:
-#             html2text_transformer = html2text.HTML2Text()
-#             transformed_documents = [
-#                 html2text_transformer.handle(document.page_content)
-#                 for document in documents
-#             ]
-#             for text in transformed_documents:
-#                 st.write(text)
+if url:
+    if ".xml" not in url:
+        with st.sidebar:
+            st.error("Please write down a Sitemap URL")
+    else:
+        documents = load_website(url, limit=100)  # Limit to 100 URLs
+        if documents:
+            for document in documents:
+                st.write(document.page_content)
--- pages/05_PrivateGPT.py
+++ pages/05_PrivateGPT.py
@@ -1,93 +1,213 @@
-import streamlit as st
-import asyncio
-import nest_asyncio
-import os
-from langchain.document_loaders import SitemapLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.prompts import ChatPromptTemplate
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
-from langchain.embeddings import OpenAIEmbeddings
-from bs4 import BeautifulSoup
+from langchain.chat_models import ChatOpenAI
+from langchain.callbacks.base import BaseCallbackHandler
+import os
+from Dark import set_page_config
 from dotenv import load_dotenv
+import streamlit as st
 from Utils import check_authentication  # Import the utility function
 
-# Set page configuration for Streamlit
+# Set the page configuration
 st.set_page_config(
-    page_title="Site",
-    page_icon="📃",
+    page_title="PrivateGPT",
+    page_icon="❓",
 )
 
+# Under construction message
+st.markdown(
+    """
+    ## This page is under construction 🔨
+    Working on it, once fixed, this page will be up and running for you to use.
+"""
+)
 
 # Ensure the user is authenticated
 check_authentication()
 
-# Load environment variables
+# Load environment variables from .env file for local development
 load_dotenv()
 
 # Access secrets in Streamlit Cloud or locally from environment variables
 openai_api_key = (
     os.getenv("OPENAI_API_KEY") or st.secrets["credentials"]["OPENAI_API_KEY"]
 )
-
-# Set PATH environment variable if specified in secrets
-if "environment" in st.secrets:
-    os.environ["PATH"] = st.secrets["environment"]["PATH"]
-
-
-# Define the page parsing function
-def parse_page(soup: BeautifulSoup):
-    for element in soup(["header", "footer", "nav", "aside", "script", "style"]):
-        element.decompose()
-    main_content = soup.find("main")
-    text = (
-        main_content.get_text(separator="\n", strip=True)
-        if main_content
-        else soup.get_text(separator="\n", strip=True)
-    )
-    return str(text).replace("\n", " ").replace("\xa0", " ")
-
-
-# Function to load and split sitemap data
-@st.cache_data(show_spinner="Loading website...")
-def load_website(url, limit=100):
-    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-        chunk_size=1000, chunk_overlap=200
-    )
-    loader = SitemapLoader(
-        url, filter_urls=[r"^(.*\/about\/).*"], parsing_function=parse_page
-    )  # Adjust filter as needed
-    loader.requests_per_second = 2
-    docs = loader.load_and_split(text_splitter=splitter)[
-        :limit
-    ]  # Limit the number of documents
-    vector_store = FAISS.from_documents(docs, OpenAIEmbeddings())
-    return vector_store.as_retriever()
-
-
-# Apply nest_asyncio to allow asyncio.run() within an existing event loop
-nest_asyncio.apply()
-
-# Set the event loop policy for Windows
-if hasattr(asyncio, "WindowsProactorEventLoopPolicy"):
-    asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
-
-# Streamlit user interface
-st.title("SiteGPT")
-st.markdown(
-    "Welcome! Use this chatbot to ask questions to an AI about the content of a website! Upload your files on the sidebar."
+alpha_vantage_api_key = (
+    os.getenv("ALPHA_VANTAGE_API_KEY")
+    or st.secrets["credentials"]["ALPHA_VANTAGE_API_KEY"]
 )
+username = os.getenv("username") or st.secrets["credentials"]["username"]
+password = os.getenv("password") or st.secrets["credentials"]["password"]
 
-with st.sidebar:
-    url = st.text_input(
-        "Write down a URL",
-        placeholder="https://www.manukau.ac.nz/about/site-help/site-map",
-    )
+# Log the API key for debugging (remove this after debugging)
+# st.write(f"OpenAI API Key: {openai_api_key}")
+# st.write(f"Alpha Vantage API Key: {alpha_vantage_api_key}")
+# st.write(f"Username: {username}")
+# st.write(f"Password: {password}")
 
-if url:
-    if ".xml" not in url:
-        with st.sidebar:
-            st.error("Please write down a Sitemap URL")
-    else:
-        documents = load_website(url, limit=100)  # Limit to 100 URLs
-        if documents:
-            for document in documents:
-                st.write(document.page_content)
+# if not openai_api_key or not alpha_vantage_api_key or not username or not password:
+#     st.error("Some required environment variables are missing.")
+#     st.stop()
+
+
+# class ChatCallBackHandler(BaseCallbackHandler):
+#     def on_llm_start(self, *args, **kwargs):
+#         self.message = ""  # Reset message on LLM start
+#         self.message_box = st.empty()  # Create an empty placeholder for the message
+
+#     def on_llm_end(self, *args, **kwargs):
+#         save_message(self.message, "ai")
+
+#     def on_llm_new_token(self, token, *args, **kwargs):
+#         self.message += token  # Accumulate tokens
+#         self.message_box.markdown(
+#             self.message
+#         )  # Update the message box with the accumulated message
+
+
+# # Log the exact usage of the API key
+# # st.write("Initializing ChatOpenAI with the provided API key...")
+# try:
+#     llm = ChatOpenAI(
+#         model="gpt-3.5-turbo",
+#         temperature=0.1,
+#         streaming=True,
+#         callbacks=[
+#             ChatCallBackHandler(),  # Use the custom callback handler
+#         ],
+#         openai_api_key=openai_api_key,  # Pass the API key here
+#     )
+#     # st.write("ChatOpenAI initialized successfully.")
+# except Exception as e:
+#     st.error(f"Failed to initialize ChatOpenAI: {e}")
+#     st.stop()
+
+
+# class SimpleMemory:
+#     def __init__(self):
+#         self.context = ""
+
+#     def update_memory(self, new_context):
+#         self.context += new_context
+
+#     def get_context(self):
+#         return self.context
+
+
+# if "memory" not in st.session_state:
+#     st.session_state.memory = SimpleMemory()
+
+
+# @st.cache_resource(show_spinner="Embedding file...")
+# def embed_file(file):
+#     file_content = file.read()
+#     # Ensure the directories exist
+#     os.makedirs("./.cache/private_files/", exist_ok=True)
+#     os.makedirs("./.cache/private_embeddings/", exist_ok=True)
+#     file_path = f"./.cache/files/{file.name}"
+#     with open(file_path, "wb") as f:
+#         f.write(file_content)
+#     cache_dir = LocalFileStore(f"./.cache/private_embeddings/{file.name}")
+#     splitter = CharacterTextSplitter.from_tiktoken_encoder(
+#         separator="\n",
+#         chunk_size=600,
+#         chunk_overlap=100,
+#     )
+#     loader = UnstructuredFileLoader(file_path)
+#     docs = loader.load_and_split(text_splitter=splitter)
+#     # st.write(
+#     #     "Embedding API Key (within embed_file):", openai_api_key=openai_api_key
+#     # )   Log the API key
+#     embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+
+#     cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+
+#     # Create a FAISS retriever
+#     retriever = FAISS.from_documents(docs, cached_embeddings)
+
+#     return retriever
+
+
+# def save_message(message, role):
+#     if "messages" not in st.session_state:  # Ensure session state has a messages list
+#         st.session_state["messages"] = []
+#     st.session_state["messages"].append({"message": message, "role": role})
+
+
+# def send_message(message, role, save=True):
+#     with st.chat_message(role):
+#         st.markdown(message)
+#     if save:
+#         save_message(message, role)
+
+
+# def paint_history():
+#     if "messages" in st.session_state:  # Check if there are messages in session state
+#         for message in st.session_state["messages"]:
+#             send_message(
+#                 message["message"],
+#                 message["role"],
+#                 save=False,
+#             )
+
+
+# def format_docs(docs):
+#     return "\n\n".join(document.page_content for document in docs)
+
+
+# prompt = ChatPromptTemplate.from_template(
+#     """
+#     Answer the question using ONLY the following context and not your training data. Understand the context and provide the answer. If you don't know the answer just say you don't know. Don't make anything up.
+
+#     Context: {context}
+#     Questions:{question}
+#     """
+# )
+
+# st.title("DocumentGPT")
+
+# st.markdown(
+#     """
+#     Welcome!
+
+#     Use this chatbot to ask questions to an AI about your files!
+
+#     Upload your files on the sidebar.
+#     """
+# )
+
+# with st.sidebar:
+#     file = st.file_uploader(
+#         "Upload a .txt .pdf or .docx file",
+#         type=["pdf", "txt", "docx"],
+#     )
+
+# if file:
+#     retriever = embed_file(file)
+#     send_message("I'm ready! Ask away!", "ai", save=False)
+#     paint_history()
+#     message = st.chat_input("Ask anything about your file...")
+#     if message:
+#         send_message(message, "human")
+#         docs = retriever.similarity_search(message)
+#         formatted_docs = format_docs(docs)
+
+#         # Update memory with the retrieved context
+#         st.session_state.memory.update_memory(formatted_docs)
+
+#         chain_input = {
+#             "context": st.session_state.memory.get_context(),
+#             "question": message,
+#         }
+
+#         chain = prompt | llm
+#         with st.chat_message("ai"):
+#             response = chain.invoke(chain_input)
+#         # No need to call send_message here as it will be handled by the callback
+# else:
+#     st.session_state["messages"] = []  # Initialize messages list if not present
